8675d40e813e573191d0db3cc50eec4fcd9a96be
minhtam220 <minhtam220@gmail.com>
2023-06-13 10:16:56 +0700

    done step9

--- src/app/config.js
+++ src/app/config.js
@@ -1,2 +1,3 @@
 //export const BASE_URL ="https://codercomm-api-dot-cs-platform-306304.et.r.appspot.com/api";
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const POST_PER_PAGE = 3;
